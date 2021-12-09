24af9b782a23b6a3a26b4418b7ceeb074f94a512
Viachaslau Abaimau <w.abaimov@gmail.com>
2021-12-09 18:29:50 +0300

    add:task#1

---
+++ 2/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let user = {
+  name: "Viachaslau",
+  surname: "Abaimov",
+  age: 29,
+  city: "Minsk",
+  isDriver: true,
+};
+
+console.log(
+  `Привет меня зовут ${user.name} ${user.surname}, мне ${user.age} лет, я живу в городе ${user.city}`
+);
