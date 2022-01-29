c853ffd13427c73fbfcb45d3ad1231760158ae58
Viachaslau Abaimau <w.abaimov@gmail.com>
2022-01-29 17:04:52 +0300

    fix:edit 1.js

--- 13/1.js
+++ 13/1.js
@@ -1,13 +1,32 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let str = "Hello world";
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13];
 
 function invert(data) {
-  let reverseArr = [];
-  for (let i = data.length - 1; i > -1; i--) {
-    reverseArr.push(data[i]);
+  if (!data || !data.length) {
+    return null;
   }
-  return reverseArr;
+
+  let result = [];
+  let numbOfIter = Math.floor((data.length - 1) / 2);
+
+  for (let i = 0; i <= numbOfIter; i++) {
+    if (Array.isArray(data)) {
+      result[data.length - 1 - i] = data[i];
+      result[i] = data[data.length - 1 - i];
+    } else {
+      let arr = data.split("");
+
+      result[arr.length - 1 - i] = arr[i];
+      result[i] = arr[arr.length - 1 - i];
+    }
+  }
+
+  if (Array.isArray(data)) {
+    return result;
+  } else {
+    return result.join("");
+  }
 }
 
-const res = invert(arr);
-
-console.log(res);
+console.log(invert(str));
+console.log(invert(arr));
